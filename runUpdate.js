be69d29f97b98152ea0f1a5f52c4af33f58ab5f7
joydasp <joydaspamula@gmail.com>
2025-12-30 16:21:10 +0530

    Clean up duplicate root backend files

--- runUpdate.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const updateArticles = require("./src/scripts/updateArticles");
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  await updateArticles();
-  console.log("All articles updated");
-  process.exit();
-});
